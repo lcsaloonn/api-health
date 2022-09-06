da6ec578eb854cf594c8a04cab738d91ef3ae347
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-09-06 12:22:53 +0200

    refactor structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './Application/auth/auth.module';
+import { indexModule } from './modules';
 import { ExerciceModule } from './modules/exercice/exercice.module';
 import { UserModule } from './modules/User/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env' }),
-    ExerciceModule,
-    UserModule,
+    indexModule,
     AuthModule,
   ],
   controllers: [AppController],
