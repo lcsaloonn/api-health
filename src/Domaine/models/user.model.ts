2cbdb1438309cbe71b80c1d0c3b659f27fe25987
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-09-10 15:17:44 +0200

    added enum role

--- src/Domaine/models/user.model.ts
+++ src/Domaine/models/user.model.ts
@@ -1,16 +1,16 @@
 import { ObjectID } from 'bson';
+import { UserRole } from '../Enums/roles.enums';
 
 export class UserModel {
   _id: ObjectID;
   username: string;
   password: string;
+  role: UserRole;
 
-  /**
-   *
-   */
-  constructor(username: string, password: string) {
+  constructor(username: string, password: string, role: UserRole) {
     this._id = new ObjectID();
     this.username = username;
     this.password = password;
+    this.role = role;
   }
 }
