4e0bf99c050ed3d51d5e3b794b3347b1403e06b4
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-09-16 13:55:16 +0200

    final

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 import { AuthModule } from './Application/auth/auth.module';
+import { AllExceptionFilter } from './exceptions/filters/allException.filter';
 import { indexModule } from './modules';
 import { ExerciceModule } from './modules/exercice/exercice.module';
 import { UserModule } from './modules/User/user.module';
@@ -11,5 +13,6 @@ import { UserModule } from './modules/User/user.module';
     indexModule,
     AuthModule,
   ],
+  providers: [],
 })
 export class AppModule {}
