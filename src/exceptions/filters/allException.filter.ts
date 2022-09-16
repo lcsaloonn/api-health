86946bbad43f958adf3312289924798e2d544314
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-09-16 10:47:11 +0200

    creation allException filter class

---
+++ src/exceptions/filters/allException.filter.ts
@@ -0,0 +1,6 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {}
+}
