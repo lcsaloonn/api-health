4bc0c7d026c2c78cfb229855a5416c44a1c2979d
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-09-30 14:39:56 +0200

    remove import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER } from '@nestjs/core';
 import { AuthModule } from './Application/auth/auth.module';
-import { AllExceptionFilter } from './exceptions/filters/allException.filter';
 import { indexModule } from './modules';
-import { ExerciceModule } from './modules/exercice/exercice.module';
-import { UserModule } from './modules/User/user.module';
 
 @Module({
   imports: [
