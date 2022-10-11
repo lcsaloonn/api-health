6e609f3dcad365986404516e567f1ded36a456b3
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-10-11 10:38:25 +0200

    new pipe isRegex

---
+++ src/Pipes/isRegex.pipe.ts
@@ -0,0 +1,34 @@
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+export function isRegex(
+  property: RegExp,
+  validationOptions?: ValidationOptions,
+) {
+  return (object: any, propertyName: string) => {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [property],
+      validator: IsRegexPipe,
+    });
+  };
+}
+
+@ValidatorConstraint({ name: 'isRegexPipe' })
+export class IsRegexPipe implements ValidatorConstraintInterface {
+  validate(value: any, validationArguments?: ValidationArguments): boolean {
+    const regex = validationArguments.constraints[0];
+    const username = validationArguments.value;
+    return regex.test(username);
+  }
+  defaultMessage?(validationArguments?: ValidationArguments): string {
+    return 'username is not good';
+  }
+}
